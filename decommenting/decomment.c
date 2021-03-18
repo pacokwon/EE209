6421eebbca15ab68cb1802edb01a6a2e46d70bbd
pacokwon <haechank@gmail.com>
2021-03-18 13:38:44 +0900

    feat(decomment): initial decomment implementation

---
+++ decommenting/decomment.c
@@ -0,0 +1,120 @@
+#include "decomment.h"
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int main(void) {
+    int c;
+
+    struct StateBox box;
+    box.currentSurrounder = '\0';
+    box.commentStartLine = 1;
+    box.currentLine = 1;
+    box.state = Start;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\n')
+            box.currentLine++;
+
+        switch (box.state) {
+        case Start:
+            opStart(c, &box);
+            break;
+        case StringLiteralContent:
+            opStringLiteralContent(c, &box);
+            break;
+        case StringLiteralBackslash:
+            opStringLiteralBackslash(c, &box);
+            break;
+        case CommentInitialBackslash:
+            opCommentInitialBackslash(c, &box);
+            break;
+        case CommentInitialAsterisk:
+            opCommentInitialAsterisk(c, &box);
+            break;
+        case CommentFinalAsterisk:
+            opCommentFinalAsterisk(c, &box);
+            break;
+        default:
+            assert(0);
+        }
+    }
+
+    if (box.state == CommentInitialAsterisk ||
+        box.state == CommentFinalAsterisk) {
+        fprintf(stderr, "Error: line %d: unterminated comment\n",
+                box.commentStartLine);
+    }
+
+    return 0;
+}
+
+void opStart(char inputChar, struct StateBox *box) {
+    if (inputChar == '/') {
+        box->state = CommentInitialBackslash;
+    } else if (inputChar == '\'' || inputChar == '"') {
+        box->currentSurrounder = inputChar;
+        box->state = StringLiteralContent;
+        printf("%c", inputChar);
+    } else {
+        printf("%c", inputChar);
+    }
+}
+
+void opStringLiteralContent(char inputChar, struct StateBox *box) {
+    char surrounder = box->currentSurrounder;
+
+    assert(surrounder != '\0');
+
+    if (inputChar == surrounder) {
+        box->currentSurrounder = '\0';
+        box->state = Start;
+    } else if (inputChar == '\\') {
+        box->state = StringLiteralBackslash;
+    } else {
+        // no-op
+    }
+
+    printf("%c", inputChar);
+}
+
+void opStringLiteralBackslash(char inputChar, struct StateBox *box) {
+    box->state = StringLiteralContent;
+    printf("%c", inputChar);
+}
+
+void opCommentInitialBackslash(char inputChar, struct StateBox *box) {
+    if (inputChar == '*') {
+        box->state = CommentInitialAsterisk;
+        box->commentStartLine = box->currentLine;
+        printf(" ");
+        return;
+    } else if (inputChar == '/') {
+        printf("/"); // print the previously unprinted slash
+    } else {
+        box->state = Start;
+        printf("/%c", inputChar);
+    }
+}
+
+void opCommentInitialAsterisk(char inputChar, struct StateBox *box) {
+    if (inputChar == '*') {
+        box->state = CommentFinalAsterisk;
+    } else if (inputChar == '\n') {
+        printf("%c", inputChar);
+    } else {
+        // no-op
+    }
+}
+
+void opCommentFinalAsterisk(char inputChar, struct StateBox *box) {
+    if (inputChar == '/') {
+        box->state = Start;
+    } else if (inputChar == '*') {
+        // no-op
+    } else if (inputChar == '\n') {
+        printf("%c", inputChar);
+    } else {
+        box->state = CommentInitialAsterisk;
+    }
+}
---
+++ decommenting/decomment.h
@@ -0,0 +1,31 @@
+#ifndef DECOMMENT_H
+#define DECOMMENT_H
+
+enum DFAState {
+    Start,
+    StringLiteralContent,
+    StringLiteralBackslash,
+    CommentInitialBackslash,
+    CommentInitialAsterisk,
+    CommentFinalAsterisk,
+};
+
+
+struct StateBox {
+    char currentSurrounder;
+    int commentStartLine;
+    int currentLine;
+    enum DFAState state;
+};
+
+void opStart(char inputChar, struct StateBox *box);
+void opStringLiteralContent(char inputChar, struct StateBox *box);
+void opStringLiteralBackslash(char inputChar, struct StateBox *box);
+void opStringLiteralEnd(char inputChar, struct StateBox *box);
+void opCommentInitialBackslash(char inputChar, struct StateBox *box);
+void opCommentInitialAsterisk(char inputChar, struct StateBox *box);
+void opCommentContent(char inputChar, struct StateBox *box);
+void opCommentFinalAsterisk(char inputChar, struct StateBox *box);
+void opCommentEnd(char inputChar, struct StateBox *box);
+
+#endif
