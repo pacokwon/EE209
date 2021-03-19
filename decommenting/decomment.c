39727bcc45903af6fec27efb8c21d274cbd4b45e
pacokwon <haechank@gmail.com>
2021-03-19 23:24:30 +0900

    docs(comments): add comments

--- decommenting/decomment.c
+++ decommenting/decomment.c
@@ -1,16 +1,28 @@
+/**
+ * Author: Haechan Kwon (권해찬)
+ * Assignment: Decommenting (Assignment 1)
+ * Filename: decomment.c
+ */
+
 #include "decomment.h"
 #include <assert.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main function
+ * Initialize the state box containing the current state.
+ * And read characters, modifying the current state until EOF
+ * If state is invalid after EOF, print error message to stderr
+ */
 int main(void) {
     int c;
-
     struct StateBox box;
     stateBoxInit(&box);
 
     while ((c = getchar()) != EOF) {
+        // keep track of current line
         if (c == '\n')
             box.currentLine++;
 
@@ -49,6 +61,12 @@ int main(void) {
     return EXIT_SUCCESS;
 }
 
+/**
+ * stateBoxInit function
+ * param box: a pointer to a struct StateBox type
+ *
+ * initialize a statebox value's fields
+ */
 void stateBoxInit(struct StateBox* box) {
     box->currentSurrounder = '\0';
     box->commentStartLine = 1;
@@ -56,10 +74,18 @@ void stateBoxInit(struct StateBox* box) {
     box->state = Start;
 }
 
+/**
+ * opStart function
+ * param inputChar: the current character that is read as input
+ * param box: a pointer to a state box
+ */
 void opStart(char inputChar, struct StateBox *box) {
     if (inputChar == '/') {
+        // transition state
+        // this is start of comment, so do not print
         box->state = CommentInitialBackslash;
     } else if (inputChar == '\'' || inputChar == '"') {
+        // update current surrounder, and transition state
         box->currentSurrounder = inputChar;
         box->state = StringLiteralContent;
         printf("%c", inputChar);
@@ -68,60 +94,95 @@ void opStart(char inputChar, struct StateBox *box) {
     }
 }
 
+/**
+ * opStringLiteralContent function
+ * param inputChar: the current character that is read as input
+ * param box: a pointer to a state box
+ */
 void opStringLiteralContent(char inputChar, struct StateBox *box) {
     char surrounder = box->currentSurrounder;
 
+    // surrounder must exist in this state
     assert(surrounder != '\0');
 
     if (inputChar == surrounder) {
+        // end of string. go back to start
         box->currentSurrounder = '\0';
         box->state = Start;
     } else if (inputChar == '\\') {
+        // escape letter inside string. transition state
         box->state = StringLiteralBackslash;
-    } else {
-        // no-op
     }
 
+    // always print string content
     printf("%c", inputChar);
 }
 
+/**
+ * opStringLiteralBackslash function
+ * param inputChar: the current character that is read as input
+ * param box: a pointer to a state box
+ */
 void opStringLiteralBackslash(char inputChar, struct StateBox *box) {
+    // accept any single character and transition state
     box->state = StringLiteralContent;
     printf("%c", inputChar);
 }
 
+/**
+ * opCommentInitialBackslash function
+ * param inputChar: the current character that is read as input
+ * param box: a pointer to a state box
+ */
 void opCommentInitialBackslash(char inputChar, struct StateBox *box) {
     if (inputChar == '*') {
+        // start of comment if * comes after /. transition state
         box->state = CommentInitialAsterisk;
+
+        // remember where this comment is starting from
         box->commentStartLine = box->currentLine;
         printf(" ");
-        return;
     } else if (inputChar == '/') {
-        printf("/"); // print the previously unprinted slash
+        // print the previously unprinted /
+        printf("/");
     } else {
+        // not a comment! print the unprinted / and character
         box->state = Start;
         printf("/%c", inputChar);
     }
 }
 
+/**
+ * opCommentInitialAsterisk function
+ * param inputChar: the current character that is read as input
+ * param box: a pointer to a state box
+ */
 void opCommentInitialAsterisk(char inputChar, struct StateBox *box) {
     if (inputChar == '*') {
+        // has potential to be end of comment. transition state
         box->state = CommentFinalAsterisk;
     } else if (inputChar == '\n') {
+        // comments are not printed. except newlines
         printf("%c", inputChar);
-    } else {
-        // no-op
     }
 }
 
+/**
+ * opCommentFinalAsterisk function
+ * param inputChar: the current character that is read as input
+ * param box: a pointer to a state box
+ */
 void opCommentFinalAsterisk(char inputChar, struct StateBox *box) {
     if (inputChar == '/') {
+        // end of comment. go back to start
         box->state = Start;
     } else if (inputChar == '*') {
-        // no-op
+        // no-op. but do not transition state
     } else if (inputChar == '\n') {
+        // comments are not printed. except newlines
         printf("%c", inputChar);
     } else {
+        // still inside comment. go back to previous state.
         box->state = CommentInitialAsterisk;
     }
 }
